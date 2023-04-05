424e836b5e6b38a14a21c2e4bb5411586082c214
kiramkumargiri <kirankumargiri145@gmail.com>
2023-04-05 22:19:51 +0545

    form submission

---
+++ login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Login Page</title>
+</head>
+<body>
+        <form action="" method="">    
+            <h1>Wel-Come To Login Page</h1>
+                <label for="username">Username: </label>
+                <input type="text" placeholder="Username" name="uname"><br><br><br>
+                <label for="password">Password: </label>
+                <input type="number" placeholder="Password" name="password"><br><br><br>
+                <div class="button">
+                <button name="submit" >Sign In</button>
+                <button class="button-submit" name="submit" >Sign Up</button>
+                </div>
+
+
+            </form>
+            <div class="imgbox">
+                <img src="" alt="">
+            </div>
+            <div>
+</body>
+</html>
